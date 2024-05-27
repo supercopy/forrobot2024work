c7b2e3fc14d2a1f0eff1d9dd371767386a9aeba7
supercopy <1347216139@qq.com>
2024-05-27 13:48:20 +0800

    again

---
+++ orangeblock.py
@@ -0,0 +1,97 @@
+import cv2
+import numpy as np
+import time
+
+class StereoVision:
+    def __init__(self, left_camera, right_camera):
+        self.capl = cv2.VideoCapture(left_camera)
+        self.capr = cv2.VideoCapture(right_camera)
+
+        # 初始化参数
+        self.kernel = np.ones((5,5), np.uint8)
+        self.kernel_open = np.ones((10,10), np.uint8)
+        self.hsv_low = np.array([13, 105, 83])
+        self.hsv_high = np.array([31, 255, 255])
+        self.clX,self.clY  = 0, 0
+        self.crX,self.crY  = 0, 0
+
+    def get_contours(self, image):
+        image = cv2.GaussianBlur(image, (5, 5), 0)
+        image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        image_dst = cv2.inRange(image_hsv, self.hsv_low, self.hsv_high)
+        image_open = cv2.morphologyEx(image_dst, cv2.MORPH_OPEN, self.kernel_open)
+        image_dilated = cv2.dilate(image_open, self.kernel, iterations=1)
+        contours, _ = cv2.findContours(image_dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        return contours, image_dilated
+    
+    #求最小外接矩形并返回顶点，使用该函数在遍历轮廓内
+    def get_rectangle(self, cnt):
+        rect = cv2.minAreaRect(cnt) #求最小外接矩形
+        box = cv2.boxPoints(rect) #返回矩形的四个顶点
+        box = np.int0(box) #转换为整型
+        return box
+
+    def get_center(self, moments):
+        centerX = int(moments["m10"] / moments["m00"])
+        centerY = int(moments["m01"] / moments["m00"])
+        return centerX, centerY
+
+    def detect_objects(self):
+        while True:
+            retl, framel = self.capl.read()
+            imagel = framel.copy()
+
+            retr, framer = self.capr.read()
+            imager = framer.copy()
+
+            contoursl, imagel_dilated = self.get_contours(imagel)
+            contoursr, imager_dilated = self.get_contours(imager)
+
+            for cntl in contoursl:
+                for cntl in contoursl:
+                    areal = cv2.contourArea(cntl)
+                    if areal < 100:
+                        continue
+                    boxl = self.get_rectangle(cntl)  # 求最小外接矩形
+                    cv2.drawContours(imagel, [boxl], 0, (255, 0, 0), 2)  # 画出矩形
+                    Ml = cv2.moments(cntl)  # 计算矩值，方便得到矩形中心点
+
+                    if Ml["m00"] != 0:
+                        reglX = self.clX
+                        reglY = self.clY
+                        self.clX, self.clY = self.get_center(Ml)  # 得到矩形中心点
+                        cv2.circle(imagel, (self.clX, self.clY), 5, (0, 0, 255), -1)  # 画出矩形中心点
+                        if self.clX != reglX or self.clY != reglY:
+                            print("左相机目标点： ", self.clX, self.clY)
+
+            for cntr in contoursr:
+                arear = cv2.contourArea(cntr)
+                if arear < 100:
+                    continue
+                boxr = self.get_rectangle(cntr)  # 求最小外接矩形
+                cv2.drawContours(imager, [boxr], 0, (255, 0, 0), 2)  # 画出矩形
+                Mr = cv2.moments(cntr)  # 计算矩值，方便得到矩形中心点
+                
+                if Mr["m00"] != 0:
+                    regrX = self.crX
+                    regrY = self.crY
+                    self.crX, self.crY = self.get_center(Mr)  # 得到矩形中心点
+                    cv2.circle(imager, (self.crX, self.crY), 5, (0, 0, 255), -1)  # 画出矩形中心点
+                    if self.crX != regrX or self.crY != regrY:
+                        print("右相机目标点： ",self.crX, self.crY)
+
+            cv2.imshow('imagel', imagel)
+            cv2.imshow('imager', imager)
+            cv2.imshow('imagel_dilated', imagel_dilated)
+            cv2.imshow('imager_dilated', imager_dilated)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        self.capl.release()
+        self.capr.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    stereo_vision = StereoVision()
+    stereo_vision.detect_objects()
--- reg.py
+++ reg.py
@@ -4,14 +4,6 @@ import time
 import random
 import math
 
-# -----------------------------------双目相机的基本参数---------------------------------------------------------
-#   left_camera_matrix          左相机的内参矩阵
-#   right_camera_matrix         右相机的内参矩阵
-#
-#   left_distortion             左相机的畸变系数    格式(K1,K2,P1,P2,0)
-#   right_distortion            右相机的畸变系数
-# -------------------------------------------------------------------------------------------------------------
-# 左镜头的内参，如焦距
 array_datal = np.array([[562.033986323064, -1.20112458901875, 368.580858376389],
                         [0, 562.529949757492, 303.887885031138],
                         [0, 0, 1]])
@@ -22,12 +14,8 @@ array_datar = np.array([[572.929758405986, -3.74046455734938, 365.816495202695],
 left_camera_matrix = array_datal.T
 right_camera_matrix = array_datar.T
 
-# 畸变系数,K1、K2、K3为径向畸变,P1、P2为切向畸变
 left_distortion = np.array([[0.124785954085824, -0.108200579559554, 0.00139760804551312, -0.00545030823545750, 0.001]])
 right_distortion = np.array([[0.0827221706506679,0.0936542340045818, 0.00862240665117579,-0.00249349828386763, 0.001]])
-
-# left_distortion = np.array([[0, 0, 0, 0, 0]])
-# right_distortion = np.array([[0, 0, 0, 0, 0]])
 
 # 旋转矩阵
 R = np.array([[0.994844339868849, -0.0345781648731146, -0.0953367187652339],
@@ -42,16 +30,11 @@ R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv2.stereoRectify(left_camera_ma
                                                                   right_camera_matrix, right_distortion, size, R,
                                                                   T)
 
-# 校正查找映射表,将原始图像和校正后的图像上的点一一对应起来
 left_map1, left_map2 = cv2.initUndistortRectifyMap(left_camera_matrix, left_distortion, R1, P1, size, cv2.CV_16SC2)
 right_map1, right_map2 = cv2.initUndistortRectifyMap(right_camera_matrix, right_distortion, R2, P2, size, cv2.CV_16SC2)
 print(len(left_map1))    
 print(Q)
 
-# --------------------------鼠标回调函数---------------------------------------------------------
-#   event               鼠标事件
-#   param               输入参数
-# -----------------------------------------------------------------------------------------------
 def onmouse_pick_points(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         threeD = param
@@ -86,7 +69,6 @@ while retl or retr:
     # 是否读取到了帧，读取到了则为True
     retl, framel = cap.read()
     retr, framer = cap1.read()
-
     # 确保两张图像形状相同
     if framel.shape == framer.shape:
     # 将两个图像叠加在一起
@@ -94,24 +76,11 @@ while retl or retr:
     # 将BGR格式转换成灰度图片，用于畸变矫正
     imgL = cv2.cvtColor(framel, cv2.COLOR_BGR2GRAY)
     imgR = cv2.cvtColor(framer, cv2.COLOR_BGR2GRAY)
-
-    # 重映射，就是把一幅图像中某位置的像素放置到另一个图片指定位置的过程。
-    # 依据MATLAB测量数据重建无畸变图片,输入图片要求为灰度图
     img1_rectified = cv2.remap(imgL, left_map1, left_map2, cv2.INTER_LINEAR)
     img2_rectified = cv2.remap(imgR, right_map1, right_map2, cv2.INTER_LINEAR)
-
     # 转换为opencv的BGR格式
     imageL = cv2.cvtColor(img1_rectified, cv2.COLOR_GRAY2BGR)
     imageR = cv2.cvtColor(img2_rectified, cv2.COLOR_GRAY2BGR)
-
-    # ------------------------------------SGBM算法----------------------------------------------------------
-    #   blockSize                   深度图成块，blocksize越低，其深度图就越零碎，0<blockSize<10
-    #   img_channels                BGR图像的颜色通道，img_channels=3，不可更改
-    #   numDisparities              SGBM感知的范围，越大生成的精度越好，速度越慢，需要被16整除，如numDisparities
-    #                               取16、32、48、64等
-    #   mode                        sgbm算法选择模式，以速度由快到慢为：STEREO_SGBM_MODE_SGBM_3WAY、
-    #                               STEREO_SGBM_MODE_HH4、STEREO_SGBM_MODE_SGBM、STEREO_SGBM_MODE_HH。精度反之
-    # ------------------------------------------------------------------------------------------------------
     blockSize = 5
     img_channels = 3
     stereo = cv2.StereoSGBM_create(minDisparity=1,
@@ -128,20 +97,16 @@ while retl or retr:
                                    )
     # 计算视差
     disparity = stereo.compute(img1_rectified, img2_rectified)
-
     # 归一化函数算法，生成深度图（灰度图）
     disp = cv2.normalize(disparity, disparity, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-
     # 生成深度图（颜色图）
     dis_color = disparity
     dis_color = cv2.normalize(dis_color, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
     dis_color = cv2.applyColorMap(dis_color, 2)
-
     # 计算三维坐标数据值
     threeD = cv2.reprojectImageTo3D(disparity, Q, handleMissingValues=True)
     # 计算出的threeD，需要乘以16，才等于现实中的距离
     threeD = threeD * 16
-
     cv2.imshow("left", framel)
     cv2.imshow("depth", dis_color)
     cv2.imshow(WIN_NAME, disp)  # 显示深度图的双目画面
@@ -150,15 +115,12 @@ while retl or retr:
     cv2.imshow("imgL", imgL)
     # 鼠标回调事件
     cv2.setMouseCallback("depth", onmouse_pick_points, threeD)
-
     #完成计时，计算帧率
     fps = (fps + (1. / (time.time() - t1))) / 2
     frame = cv2.putText(framel, "fps= %.2f" % (fps), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
     # 若键盘按下q则退出播放
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
-
 # 释放资源
 cap.release()
 cap1.release()
